1525a2da9dfbcb5f4970c842e9a3b635c63c67ac
kelaompachai <141376761+kelaompachai@users.noreply.github.com>
2024-11-21 23:42:17 -0500

    write script to create initial database and users table

--- server/models/build-database.sql
+++ server/models/build-database.sql
@@ -6,8 +6,15 @@ CREATE ROLE passtheplate WITH LOGIN PASSWORD 'cheese';
 -- create a database owned by that user
 CREATE DATABASE passtheplate OWNER passtheplate;
 
+-- switch context to new database
+\connect passtheplate passtheplate
 
 -- create users table
 -- fields include username, password, and zip code
 -- username must be unique, so it should be a sufficient primary key
 
+CREATE TABLE users (
+  username VARCHAR(15) PRIMARY KEY,
+  password VARCHAR(15) NOT NULL,
+  zipcode VARCHAR(5) NOT NULL
+);
